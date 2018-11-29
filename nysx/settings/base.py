0b89c36c6e129e0a9dde02ae4f4f979906563159
Cormac Collins <cormaccollins90@gmail.com>
2018-11-29 16:02:13 +0000

    settings update

--- nysx/settings/base.py
+++ nysx/settings/base.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['nysx-cormaccollins90.c9users.io']
+ALLOWED_HOSTS = ['nysx-cormaccollins90.c9users.io', 'com-nysx-django-app.herokuapp.com']
 
 
 # Application definition
